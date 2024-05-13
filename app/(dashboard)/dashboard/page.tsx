7aafa70c621c9da86eb369ba8fc726e48325337e
pmita <panos.mita2@outlook.com>
2024-05-13 23:20:07 +0100

    feat(dashboard): displaying either row or grid format

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,15 +2,29 @@
 import { getCollectionData } from '@/data/firestore';
 // COMPONENTS
 import { TableContainer } from '../_components/table-container';
+import { ContainerOptions } from '../_components/container-options';
+import { GridContainer } from '../_components/grid-container';
 
 
 export default async function DashboardPage({ searchParams }: { searchParams: any }) {
   // SERVER LAND
   const data = await getCollectionData('queries', searchParams);
 
+  console.log(data);
+
   return (
-    <div className="overflow-x-auto">
-      <TableContainer data={data} /> 
-    </div>
+    <>
+    <ContainerOptions displayType={searchParams.display} />
+
+    {searchParams.display === 'grid' && (
+      <GridContainer data={data} />
+    )}
+
+    {searchParams.display === 'list' && (
+      <div className="overflow-x-auto">
+        <TableContainer data={data} /> 
+      </div>
+    )}
+    </>
   )
 }
