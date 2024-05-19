0816247923d897c2620f2daf41eecf29f6f982c4
panosVegas <panos.mita@leovegas.com>
2024-05-19 11:18:42 +0100

    chore: testing permissions

--- app/(dashboard)/_components/table-container.tsx
+++ app/(dashboard)/_components/table-container.tsx
@@ -42,7 +42,7 @@ export const TableRow = ({
   email: string;
   createdAt: any;
 }) => {
-
+  console.log('test')
   return (
     <tr>
       <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{name}</td>
