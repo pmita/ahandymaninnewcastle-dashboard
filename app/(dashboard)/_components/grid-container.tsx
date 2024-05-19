4147349117717a844e0ad85c2df5e348053bb055
panosVegas <panos.mita@leovegas.com>
2024-05-19 11:27:25 +0100

    style(grid): fix spacing issues

--- app/(dashboard)/_components/grid-container.tsx
+++ app/(dashboard)/_components/grid-container.tsx
@@ -9,17 +9,17 @@ import { cn, truncate } from "@/utils/helpers";
 
 export const GridContainer = ({ data}: { data: any }) => {
   return (
-    <div className="bg-secondary grid grid-cols-1 grid-rows-[350px] gap-4 lg:grid-cols-2 lg:gap-8 min-h-[100dvh] outline-dashed">
+    <div className="bg-secondary grid grid-cols-1 grid-rows-[300px] gap-4 lg:grid-cols-2 lg:gap-8 min-h-[100dvh] mt-4 p-4">
       {data.map((item: any) => (
-        <Card key={item.id}>
-          <CardHeader className="flex-row justify-between items-center">
+        <Card key={item.id} className="flex flex-col justify-center items-stretch">
+          <CardHeader className="flex-1 flex-row justify-between items-center">
               <Status status={item.status} />
               <FormatedTime time={item.createdAt} />
           </CardHeader>
-          <CardContent>
+          <CardContent className="flex-1">
             {truncate(item.additionalInfo, 150)}
           </CardContent>
-          <CardFooter className="self-end flex-col justify-center items-stretch gap-2">
+          <CardFooter className="flex-1 flex-col justify-center items-stretch gap-2">
             <Button
               className={cn(buttonVariants({ variant: 'primary', size: 'sm' }))}
             >
