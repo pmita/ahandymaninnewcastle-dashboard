b6847cd6a3d017b473d1df992c6349860ddfc508
panosVegas <panos.mita@leovegas.com>
2024-06-28 23:15:36 +0100

    refactor(dashboard): rendering real time container in the client page

--- app/(dashboard)/dashboard/_components/realtime-items.tsx
+++ app/(dashboard)/dashboard/_components/realtime-items.tsx
@@ -2,23 +2,20 @@
 
 // REACT
 import { useState, useCallback } from "react"
-import { GridContainer } from "./grid-container"
+// COMPONENTS
 import { Button, buttonVariants } from "@/components/ui/button"
+import { ToggleContainer } from "./toggle-container"
 // FIREBASE
 import { firestore, fromMillis } from "@/firebase/client-config"
 // UTILS
 import { cn } from "@/utils/helpers"
 // TYPES
-import { IFirestoreItem, queryDocumentType } from "@/types/firestore"
-
-type RealtimeItemPropsType = {
-  data: queryDocumentType[] | null;
-}
+import { IFirestoreItem } from "@/types/firestore"
 
 const TEN_MORE_ITEMS = 10;
 
-export const RealtimeItems = ({ data }: RealtimeItemPropsType ) => {
-    const [items, setItems] = useState<queryDocumentType[] | null>(data);
+export const RealtimeItems = ({ data }: { data: IFirestoreItem[] | null } ) => {
+    const [items, setItems] = useState<IFirestoreItem[] | null>(data);
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
     const [error, setError] = useState<Error | string | null>(null);
@@ -64,21 +61,23 @@ export const RealtimeItems = ({ data }: RealtimeItemPropsType ) => {
     }, [items]);
 
     return (
-        <section className="flex flex-col justify-center items-center gap-4">
-            <GridContainer data={items} />
-            {hasMore 
-            ? (
-                <Button
-                    className={cn(buttonVariants({ variant: 'primary', size: 'sm' }), "text-center")}
-                    onClick={loadMoreItems}
-                    disabled={loading || !hasMore}
-                >
-                    {loading ? 'Loading...' : 'Load More'}
-                </Button>
-            ): (
-                <p className="text-center text-neutral">No more items to load</p>
-            )}
-            
-        </section>
+        <>
+            <ToggleContainer data={items} />
+
+            <section className="flex flex-col justify-center items-center pt-5">
+                {hasMore 
+                ? (
+                    <Button
+                        className={cn(buttonVariants({ variant: 'primary', size: 'sm' }), "text-center")}
+                        onClick={loadMoreItems}
+                        disabled={loading || !hasMore}
+                    >
+                        {loading ? 'Loading...' : 'Load More'}
+                    </Button>
+                ): (
+                    <p className="text-center text-neutral">No more items to load</p>
+                )}
+            </section>    
+        </>
     )
 }
