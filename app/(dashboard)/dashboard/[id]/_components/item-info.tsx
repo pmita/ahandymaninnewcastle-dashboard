b25653ff7611327f1f1cffd2f2e09b7bc93720a1
panosVegas <panos.mita@leovegas.com>
2024-07-06 16:57:10 +0100

    chore: clean coding ./dashboard/[id]/_component folder

--- app/(dashboard)/dashboard/[id]/_components/item-info.tsx
+++ app/(dashboard)/dashboard/[id]/_components/item-info.tsx
@@ -1,5 +1,6 @@
 // COMPONENTS
 import { Card, CardDescription } from "@/components/ui/card";
+// TYPES
 import { IFirestoreItem } from "@/types/firestore";
 
 export const ItemInfo = ({
@@ -7,16 +8,14 @@ export const ItemInfo = ({
 }: { itemData: IFirestoreItem}) => {
 
   return (
-    <div className="rounded-lg bg-secondary flex flex-col justify-center items-stretch p-2 lg:p-4">
-      <Card>
-        <CardDescription>
-          <h1>Name: {itemData?.fullName}</h1>
-          <h1>Email: {itemData?.email}</h1>
-          <h1>Mobile: {itemData?.mobile}</h1>
-          {itemData.location && <h1>Location: {itemData.location}</h1>}
-          {itemData.additionalInfo && <p>Details: {itemData.additionalInfo}</p>}
-        </CardDescription>
-      </Card>
-    </div>
+    <Card className="rounded-lg bg-secondary flex flex-col justify-center items-stretch p-2 lg:p-4">
+      <CardDescription>
+        <h1>Name: {itemData?.fullName}</h1>
+        <h1>Email: {itemData?.email}</h1>
+        <h1>Mobile: {itemData?.mobile}</h1>
+        {itemData.location && <h1>Location: {itemData.location}</h1>}
+        {itemData.additionalInfo && <p>Details: {itemData.additionalInfo}</p>}
+      </CardDescription>
+    </Card>
   )
 }
